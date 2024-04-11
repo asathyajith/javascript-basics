8475eb5132f45e427e843b508c0b8ae4140eaae9
asathyjith <asathyajith98@gmail.com>
2024-04-11 18:38:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const a = 3;
+const b = {};
+
+//cookies
+const cookies = document.cookie; // Get all cookies
+document.cookie = 'username=john; expires=' + expirationDate.toUTCString();
+
+//Date methods in JS
+
+//promises 
+function xyz(){
+    function abc(){};
+    abc.then(
+        //true
+    ).catch(
+        //false
+    );
+};
+
+//checking integer 
+function isInt(num) {
+    return num % 1 == 0;
+  }  
+console.log(isInt(4));
+console.log(isInt(12.2));
+console.log(isInt(0.3));
+  
+const xv = { a: 1, b: 2 };
