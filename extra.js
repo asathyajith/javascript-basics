28f7c03b82ef45c2832b0386334bacc239a8746a
asathyjith <asathyajith98@gmail.com>
2024-04-13 18:12:08 +0530

    change

---
+++ extra.js
@@ -0,0 +1,14 @@
+
+// typeof {name:'John', age:34} // Returns "object"
+// typeof [1,2,3,4]             // Returns "object" (not "array", see note below)
+// typeof null                  // Returns "object"
+
+// The data type of an undefined variable is undefined *
+
+//try and catch,finally block - Best practice
+
+
+////ERROR////
+// ReferenceError	An illegal reference has occurred
+// SyntaxError	A syntax error has occurred
+// TypeError	A type error has occurred
--- index.js
+++ index.js
@@ -23,6 +23,20 @@
 //math.random - for generating random numbers OTPs ect...
 // Math.floor(Math.random() * 101); //numbers 0 to 100
 
+/*
+//##Advantages of Arrow Functions//
+~introduced in ES6
+~Arrow functions reduce the size of the code.
+~The return statement and function brackets are optional for single-line functions.
+~It increases the readability of the code.
+~Arrow functions provide a lexical this binding. It means, they inherit the value of “this” from the enclosing scope. This feature can be advantageous when dealing with event listeners or callback functions where the value of “this” can be uncertain.
+//##Limitations of Arrow Functions//
+~Arrow functions do not have the prototype property.
+~Arrow functions cannot be used with the new keyword.
+~Arrow functions cannot be used as constructors.
+~These functions are anonymous and it is hard to debug the code.
+~Arrow functions cannot be used as generator functions that use the yield keyword to return multiple values over time.
+*/
 
 //promises//
 // function xyz(){
