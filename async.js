5248513c0e81df1078ffdb3747bd1c8269a700f8
asathyjith <asathyajith98@gmail.com>
2024-04-14 11:44:56 +0530

    change

---
+++ async.js
@@ -0,0 +1,3 @@
+/*JavaScript functions are executed in the sequence they are called. Not in the sequence they are defined.*/
+
+/*https://medium.com/@gopesh.jangid/what-is-the-between-browser-event-loop-and-node-event-loop-explained-cf3bbb981545#:~:text=While%20the%20JavaScript%20event%20loop,threading%20through%20a%20thread%20pool.*/
