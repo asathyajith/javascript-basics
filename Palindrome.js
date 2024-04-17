b1db6c8191289e5439db98d5de4f5eb341dc2a43
asathyjith <asathyajith98@gmail.com>
2024-04-17 12:08:01 +0530

    Palindrome

---
+++ Palindrome.js
@@ -0,0 +1,20 @@
+
+
+function xyz (arr){
+
+    arr.forEach(ele => {
+        let sum = 0 ;
+        if(ele > 0){
+            sum = sum + ele
+        }
+        return sum;
+    });
+
+}
+
+
+function plaindrome_check (str){
+
+    str.split().reverse().join()  
+
+}
